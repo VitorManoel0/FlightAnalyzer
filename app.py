1f218197f9e83afaea9c3cfbcd09bae7495ea228
Vitor <vmanoel_silva@outlook.com>
2023-10-05 02:26:11 -0400

    wip

--- app.py
+++ app.py
@@ -139,5 +139,10 @@ def logout():
     return redirect('/')
 
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
 if __name__ == '__main__':
     app.run(debug=True)
--- conexao.py
+++ conexao.py
@@ -1,6 +1,5 @@
 import os
 from flask import Flask
-from flask_jwt_extended import JWTManager
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -21,15 +20,6 @@ def init_app(app=None):
     db = SQLAlchemy(app)
 
     return db
-
-
-def config_jwt():
-
-    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
-    app.config['JWT_SECRET_KEY'] = JWT_SECRET_KEY
-    jwt = JWTManager(app)
-
-    return jwt
 
 
 db = init_app(app)
